caa324d5a705e9a1e2b21d608cadf71bda685b1e
LeThanhCong <ltchcmus@gmail.com>
2025-11-04 20:30:49 +0700

    Add HEAD /check endpoint to all services for health monitoring

---
+++ src/backend/backendSocial/post/src/main/java/service/post/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package service.post.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping(value = "/check", method = RequestMethod.HEAD)
+    public ResponseEntity<Void> checkHealth() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
